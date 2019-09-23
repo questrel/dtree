1988a69ff5943d51cbc3f713e7620bcf90909a78
dmi-000 <55475423+dmi-000@users.noreply.github.com>
2019-09-23 12:05:05 -0700

    create test data if needed

---
+++ test.bash
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+if [[ ! -e sql.dat.99 ]] ; then 
+   head -99 /usr/share/dict/words | chris/canonical.out -S | ./sql.test.out
+   cp -av sql.dat sql.dat.99
+fi
+cp -av  sql.dat.99 sql.dat # restore default
+rm *.map # shared map not working
+(echo 'select column(1),NUMERIC_TO_STRING(column(9),column(10)) from word where column(9)*column(10) <= column(9)+ column(10);' ; cat ) | ./sql.test.out
